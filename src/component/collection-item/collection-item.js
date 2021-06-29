94d62feb0333054da4651323cdda715b8360318f
Sanskar00 <saxenasanskar090901@gmail.com>
2021-06-30 04:10:03 +0530

    added card-dropdown cart-icon and cart-item component to our app

--- src/component/collection-item/collection-item.js
+++ src/component/collection-item/collection-item.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
+import CustomButton from '../custom-button/custom-button';
+
+import { connect } from 'react-redux';
+
+import { addItem } from '../../redux/cart/cart.action';
+
 import './collection-item.scss'
 
-const CollectionItem=({id ,name,price,imageUrl})=>(
+const CollectionItem=({item,addItem})=>{
+    const {name,price,imageUrl}=item;
+    return(
     <div className='collection-item'>
         <div
         className='image'
@@ -14,9 +22,13 @@ const CollectionItem=({id ,name,price,imageUrl})=>(
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
     </div>
+    <CustomButton  onClick={()=>addItem(item)} inverted> Add to cart </CustomButton>
 
         
-    </div>
-);
+    </div>)
+};
+const mapDispatchToProps=dispatch=>({
+    addItem: item => dispatch(addItem(item))
+})
 
-export default CollectionItem;
+export default connect(null,mapDispatchToProps)(CollectionItem);
